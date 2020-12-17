0593b8b4bf7e7e11a056a0ccb31fb275966a5cf6
kevin <kiariekevin22@gmail.com>
2020-12-17 08:57:19 +0300

    safe url added

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, get_object_or_404,Http404,redirect
 from django.http import HttpResponse, JsonResponse
+from django.utils.http import is_safe_url
+from django.conf import settings
 from .models import Tweet
 import random
 from .forms import Tweetform
 # Create your views here.
+
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 def home_view(request):
     return render(request, 'pages/home.html')
 
@@ -14,7 +18,7 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        if next_url != None:
+        if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = Tweetform()
     return render(request, 'components/form.html', context={'form':form} )
