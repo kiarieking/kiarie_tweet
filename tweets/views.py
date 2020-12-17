bccd5c7e0f98798be3e8f33f3d30e9f889a3e309
kevin <kiariekevin22@gmail.com>
2020-12-17 06:00:25 +0300

    added navbar and formarted tweets with bootstrap

--- tweets/views.py
+++ tweets/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, get_object_or_404,Http404
 from django.http import HttpResponse, JsonResponse
 from .models import Tweet
+import random
 # Create your views here.
 def home_view(request):
     return render(request, 'pages/home.html')
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id, "content":x.content}for x in qs]
+    tweets_list = [{"id":x.id, "content":x.content, "likes":random.randint(0,1001)}for x in qs]
     data = {
         "response":tweets_list
     }
