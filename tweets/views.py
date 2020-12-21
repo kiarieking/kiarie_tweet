e23b72315269346dd744fbd3ee48a52c701bf909
kevin <kiariekevin22@gmail.com>
2020-12-21 05:34:15 +0300

    serialized tweet model

--- tweets/views.py
+++ tweets/views.py
@@ -18,6 +18,8 @@ def tweet_create_view(request, *args, **kwargs):
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
         if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = Tweetform()
@@ -25,7 +27,7 @@ def tweet_create_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id, "content":x.content, "likes":random.randint(0,1001)}for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "response":tweets_list
     }
