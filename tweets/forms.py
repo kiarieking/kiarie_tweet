635526acb782ec05e2add78080bebdd6d4f06e21
kevin <kiariekevin22@gmail.com>
2020-12-28 18:47:34 +0300

    error handling

--- tweets/forms.py
+++ tweets/forms.py
@@ -8,7 +8,7 @@ class Tweetform(forms.ModelForm):
         model = Tweet
         fields = ['content']
 
-    def clean_tweet(self):
+    def clean_content(self):
         content = self.cleaned_data.get('content') 
         if len(content) > MAX_TWEET_LENGTH:
             raise forms.ValidationError('This tweet is too long') 
