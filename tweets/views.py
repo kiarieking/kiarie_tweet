f150951671d81f2f1d5f21173fd13d309c3404a4
kevin <kiariekevin22@gmail.com>
2021-01-04 06:53:03 +0300

    tweet detail view serializer

--- tweets/views.py
+++ tweets/views.py
@@ -25,11 +25,22 @@ def tweet_create_view(request, *args, **kwargs):
         return Response(serializer.data, status=201)
     return Response({}, status=400)
 
+
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data, )    
+
+
+@api_view(['GET'])
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    obj = qs.first()
+    serializer = TweetSerializer(obj)
+    return Response(serializer.data)
 
 def tweet_create_view_Django(request, *args, **kwargs):
     user = request.user
@@ -64,7 +75,7 @@ def tweet_list_view_pure_Django(request, *args, **kwargs):
     return JsonResponse(data)
 
 
-def tweet_detail_view(request,tweet_id, *args, **kwargs):
+def tweet_detail_view_pure_Django(request,tweet_id, *args, **kwargs):
     data = {"id":tweet_id}
     try:
         obj = Tweet.objects.get(id=tweet_id)
